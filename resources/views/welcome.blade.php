f3b7b19eb30717c820452fa111f53358931dbbc8
muhbase <148293524+felda2@users.noreply.github.com>
2025-01-07 10:53:13 +0700

    Resume Project Web Programming Laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,278 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>M Bagas | Resume</title>
+    <meta name="description" content="Bako - Personal Portfolio & Resume HTML Template">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="images/favicon.png">
+
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" type="text/css" media="all">
+    <link rel="stylesheet" href="{{ asset('css/all.min.css') }}" type="text/css" media="all">
+    <link rel="stylesheet" href="{{ asset('css/simple-line-icons.css') }}" type="text/css" media="all">
+    <link rel="stylesheet" href="{{ asset('css/slick.css') }}" type="text/css" media="all">
+    <link rel="stylesheet" href="{{ asset('css/jquery.mCustomScrollbar.min.css') }}" type="text/css" media="all">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" type="text/css" media="all">
+</head>
+
+<body>
+
+    <!-- preloader -->
+    <div id="preloader">
+        <div class="outer">
+            <div class="spinner">
+                <div class="dot1"></div>
+                <div class="dot2"></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="site-wrapper">
+        <div class="mobile-header py-2 px-3 mt-4">
+            <button class="menu-icon mr-2">
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+            <a href="index-2.html" class="logo"><img src="images/hero.jpg" style="width: 60px; border-radius: 50%; alt="M Bagas" /></a>
+            <a href="index-2.html" class="site-title dot ml-2">M Bagas</a>
+        </div>
+
+        <header class="left float-left shadow-dark" id="header">
+            <button type="button" class="close" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            <div class="header-inner d-flex align-items-start flex-column">
+                <a href="index-2.html"><img src="images/hero.jpg" alt="M Bagas"
+                        style="width: 120px; border-radius: 50%;" /></a>
+                <a href="index-2.html" class="site-title dot mt-3">M. Bagas</a>
+                <span class="site-slogan">Mahasiswa</span>
+
+                <nav>
+                    <ul class="vertical-menu scrollspy">
+                        <li><a href="#home" class="active"><i class="icon-home"></i>Beranda</a></li>
+                        <li><a href="#about"><i class="icon-user"></i>Tentang Saya</a></li>
+                        <li><a href="#resume"><i class="icon-graduation"></i>Profile</a></li>
+                        <li><a href="#contact"><i class="icon-phone"></i>Kontak</a></li>
+                    </ul>
+                </nav>
+                <div class="footer mt-auto">
+                    <ul class="social-icons list-inline">
+                        <li class="list-inline-item"><a href="https://www.instagram.com/vanzgass/" target="_blank"><i
+                                    class="fab fa-instagram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+
+        <main class="content float-right">
+            <section class="hero background parallax shadow-dark d-flex align-items-center" id="home" data-image-src="images/hero.jpg">
+                <div class="cta mx-auto mt-2">
+                    <h1 class="mt-0 mb-4">Saya M Bagas<span class="dot"></span></h1>
+                    <p class="mb-4">Saya adalah mahasiswa di STIMIK Widya Utama, dengan spesialisasi dalam pemrograman web. Bersemangat dalam merancang situs web dan aplikasi modern yang responsif.</p>
+                    <a href="#" class="btn btn-default btn-lg mr-3"><i class="icon-grid"></i>Lihat Portofolio</a>
+                    <div class="spacer d-md-none d-lg-none d-sm-none" data-height="10"></div>
+                </div>
+                <div class="overlay"></div>
+            </section>
+            
+
+
+            <!-- section about -->
+            <section id="about" class="shadow-blue white-bg padding">
+                <h3 class="section-title">Tentang Saya</h3>
+                <div class="spacer" data-height="80"></div>
+
+                <div class="row">
+                    <div class="col-md-3">
+                        <img src="images/hero.jpg" style="width: 120px; border-radius: 50%;" alt="tentang" />
+                    </div>
+                    <div class="col-md-9">
+                        <h2 class="mt-4 mt-md-0 mb-4">Halo,</h2>
+                        <p class="mb-0">Saya Muhammad Bagas, seorang mahasiswa pemrograman web dari STIMIK Widya
+                            Utama. Saya memiliki minat yang kuat dalam merancang dan membangun situs web serta aplikasi
+                            modern yang responsif.</p>
+                        <div class="row my-4">
+                            <div class="col-md-6">
+                                <p class="mb-2">Nama: <span class="text-dark">Muhammad Bagas</span></p>
+                                <p class="mb-0">Nomor Telepon: <span class="text-dark">08985176460</span></p>
+                            </div>
+                            <div class="col-md-6 mt-2 mt-md-0 mt-sm-2">
+                                <p class="mb-2">Lokasi: <span class="text-dark">Indonesia</span></p>
+                                <p class="mb-0">Email: <span class="text-dark">bgasset67@gmail.com</span></p>
+                            </div>
+                        </div>
+                        <a href="#" class="btn btn-default mr-3"><i class="icon-cloud-download"></i>Unduh
+                            CV</a>
+                    </div>
+                </div>
+            </section>
+
+
+            <!-- section experience -->
+            <section id="profile" class="shadow-blue white-bg padding">
+                <h3 class="section-title">Profil</h3>
+                <div class="spacer" data-height="80"></div>
+
+                <!-- timeline riwayat pendidikan -->
+                <div class="timeline">
+                    <!-- Universitas -->
+                    <div class="entry">
+                        <div class="title">
+                            <span>2021 - Sekarang</span>
+                        </div>
+                        <div class="body">
+                            <h4 class="mt-0">Mahasiswa Web Programming</h4>
+                            <p>Saat ini sedang menempuh pendidikan di STIMIK Widya Utama dengan jurusan Web Programming,
+                                mempelajari teknologi dan praktik pengembangan web modern.</p>
+                        </div>
+                    </div>
+
+                    <!-- SMA -->
+                    <div class="entry">
+                        <div class="title">
+                            <span>2018 - 2021</span>
+                        </div>
+                        <div class="body">
+                            <h4 class="mt-0">SMK YPT 2 Purbalingga</h4>
+                            <p>Lulus dari SMK YPT 2 Purbalingga, di mana saya mulai tertarik pada teknologi dan ilmu
+                                komputer, yang menjadi dasar untuk studi saya selanjutnya.</p>
+                        </div>
+                    </div>
+
+                    <!-- SMP -->
+                    <div class="entry">
+                        <div class="title">
+                            <span>2015 - 2018</span>
+                        </div>
+                        <div class="body">
+                            <h4 class="mt-0">SMP negeri 5 Purbalingga</h4>
+                            <p>Menyelesaikan pendidikan SMP di SMP negeri 5 Purbalingga, di mana saya mulai
+                                mengeksplorasi pemrograman dasar dan teknologi, yang memicu ketertarikan saya dalam
+                                pengembangan web.</p>
+                        </div>
+                    </div>
+
+                    <span class="timeline-line"></span>
+                </div>
+            </section>
+
+            <section id="contact" class="shadow-blue white-bg padding">
+                <h3 class="section-title">Hubungi Saya</h3>
+                <div class="spacer" data-height="80"></div>
+
+                <div class="row">
+
+                    <div class="col-md-4 mb-4 mb-md-0">
+                        <!-- info kontak -->
+                        <div class="contact-info mb-5">
+                            <i class="icon-phone"></i>
+                            <div class="details">
+                                <h5>Telepon</h5>
+                                <span>08985176460</span>
+                            </div>
+                        </div>
+                        <div class="contact-info mb-5">
+                            <i class="icon-envelope"></i>
+                            <div class="details">
+                                <h5>Alamat Email</h5>
+                                <span>bgasset67@gmail.com</span>
+                            </div>
+                        </div>
+                        <div class="contact-info">
+                            <i class="icon-location-pin"></i>
+                            <div class="details">
+                                <h5>Lokasi</h5>
+                                <span>Purwodadi RT 01/01 Purbalingga Lor</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-8">
+                        <!-- Form Kontak -->
+                        <form id="contact-form" class="contact-form" method="post" action="">
+
+                            <div class="messages"></div>
+
+                            <div class="row">
+                                <div class="column col-md-6">
+                                    <!-- Input Nama -->
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="InputName" id="InputName"
+                                            placeholder="Nama Anda" required="required"
+                                            data-error="Nama wajib diisi.">
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+                                </div>
+
+                                <div class="column col-md-6">
+                                    <!-- Input Email -->
+                                    <div class="form-group">
+                                        <input type="email" class="form-control" id="InputEmail" name="InputEmail"
+                                            placeholder="Alamat Email" required="required"
+                                            data-error="Email wajib diisi.">
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+                                </div>
+
+                                <div class="column col-md-12">
+                                    <!-- Input Subjek -->
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" id="InputSubject"
+                                            name="InputSubject" placeholder="Subjek" required="required"
+                                            data-error="Subjek wajib diisi.">
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+                                </div>
+
+                                <div class="column col-md-12">
+                                    <!-- Input Pesan -->
+                                    <div class="form-group">
+                                        <textarea name="InputMessage" id="InputMessage" class="form-control" rows="5" placeholder="Pesan"
+                                            required="required" data-error="Pesan wajib diisi."></textarea>
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <button type="submit" name="submit" id="submit" value="Submit"
+                                class="btn btn-default">
+                                <i class="icon-paper-plane"></i>Kirim Pesan
+                            </button><!-- Tombol Kirim -->
+
+                        </form>
+                        <!-- Akhir Form Kontak -->
+                    </div>
+
+                </div>
+
+            </section>
+
+
+        </main>
+
+    </div>
+
+    <!-- Go to top button -->
+    <a href="javascript:" id="return-to-top"><i class="fa fa-chevron-up"></i></a>
+
+    <!-- SCRIPTS -->
+    <script src="{{ asset('js/jquery-1.12.3.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.easing.min.js') }}"></script>
+    <script src="{{ asset('js/popper.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.waypoints.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.counterup.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.mCustomScrollbar.concat.min.js') }}"></script>
+    <script src="{{ asset('js/isotope.pkgd.min.js') }}"></script>
+    <script src="{{ asset('js/infinite-scroll.min.js') }}"></script>
+    <script src="{{ asset('js/imagesloaded.pkgd.min.js') }}"></script>
+    <script src="{{ asset('js/slick.min.js') }}"></script>
+    <script src="{{ asset('js/contact.js') }}"></script>
+    <script src="{{ asset('js/validator.js') }}"></script>
+    <script src="{{ asset('js/custom.js') }}"></script>
+
+</body>
+
+</html>
